676540319dfd569b1fcdbc69bad39c4ea2cdc704
RainGu <AlphaRG@192.168.0.100>
2017-08-24 01:25:09 +0800

    add

---
+++ rgwork/rgwork/rgwork.h
@@ -0,0 +1,19 @@
+//
+//  rgwork.h
+//  rgwork
+//
+//  Created by RainGu on 2017/8/24.
+//  Copyright © 2017年 AlphaRG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for rgwork.
+FOUNDATION_EXPORT double rgworkVersionNumber;
+
+//! Project version string for rgwork.
+FOUNDATION_EXPORT const unsigned char rgworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <rgwork/PublicHeader.h>
+
+
